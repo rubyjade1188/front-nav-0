3d78e64658715588461b956b34063051b1fb794e
sallyzhou <sallymjz1188@gmail.com>
2022-01-09 12:56:21 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,83 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+	{ logo: "A", logoType: "text", url: "https://www.acfun.cn/" },
+	{
+		logo: "B",
+		logoType: "image",
+		url: "https://www.bilibili.com/",
+	},
+];
+
+const simplifyUrl = (url) => {
+	return url
+		.replace("https://", "")
+		.replace("http://", "")
+		.replace("www.", "")
+		.replace(/\/.*/, "");
+};
+
+const render = () => {
+	$siteList.find("li:not(.last)").remove();
+	hashMap.forEach((node, index) => {
+		const $li = $(`<li>
+                <div class="site">
+                    <div class="logo">${node.logo[0]}</div>
+                    <div class="link">${simplifyUrl(node.url)}</div>
+                    <div class="close">
+                        <svg class="icon">
+                            <use xlink:href="#icon-blue-square-fill-copy"></use>
+                        </svg>
+                    </div>
+                </div>
+
+        </li>`).insertBefore($lastLi);
+
+		$li.on("click", () => {
+			window.open(node.url);
+		});
+		$li.on("click", ".close", (e) => {
+			console.log("这里");
+			e.stopPropagation(); //阻止冒泡
+			console.log(hashMap);
+			hashMap.splice(index, 1);
+			render();
+		});
+	});
+};
+render();
+
+$(".addButton").on("click", () => {
+	let url = window.prompt("请问你要添加的网址是啥？");
+
+	if (url.indexOf("http") !== 0) {
+		url = "https://" + url;
+	}
+	console.log(url);
+	hashMap.push({
+		logo: simplifyUrl(url)[0].toUpperCase(),
+		logoType: "text",
+		url: url,
+	});
+	render();
+});
+
+window.onbeforeunload = () => {
+	//localStorage只能存字符串;
+	const string = JSON.stringify(hashMap);
+	localStorage.setItem("x", string);
+};
+
+//键盘导航
+$(document).on("keypress", (e) => {
+	// const key = e.key;
+	const { key } = e;
+	for (let i = 0; i < hashMap.length; i++) {
+		if (hashMap[i].logo.toLowerCase() === key) {
+			window.open(hashMap[i].url);
+		}
+	}
+});
